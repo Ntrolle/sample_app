85be410dd09c0acf2c313d6b5f5d0e28a841398e
Joe Bar <ntrolle@hotmail.com>
2013-03-21 07:35:04 +0100

    Excercices of chapter 3 done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Returning a title based on the page
+  def title
+    base_title = "Ruby on Rails Tutorial - Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
